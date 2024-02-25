cb24065782375a97a6af07e7658421f67b0ce8e5
moebiusmania <moebiusmania@gmail.com>
2024-02-25 18:19:20 +0100

    chore: updating dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,8 @@ module.exports = {
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: ["corporate", "dark"],
+    darkTheme: "dark",
+  },
 };
