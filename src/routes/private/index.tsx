a5e903463a5ac4c15d6a0c6b3b3f4bb2109dd314
moebiusmania <moebiusmania@gmail.com>
2024-03-03 14:58:42 +0100

    feat: adding env check in private route

--- src/routes/private/index.tsx
+++ src/routes/private/index.tsx
@@ -1,5 +1,5 @@
-import type { DocumentHead } from "@builder.io/qwik-city";
-import { component$ } from "@builder.io/qwik";
+import { server$, type DocumentHead } from "@builder.io/qwik-city";
+import { component$, $, useSignal } from "@builder.io/qwik";
 
 // import Conversation from "~/components/Conversation";
 // import { parse } from "~/utils/parse";
@@ -7,9 +7,21 @@ import { component$ } from "@builder.io/qwik";
 import pkg from "../../../package.json";
 
 export default component$(() => {
+  const logged = useSignal<boolean>(false);
+
+  const checkPswd = server$(function (pswd: string) {
+    return pswd === this.env.get("PASSWORD");
+  });
+
+  const onInput = $(async (event: any) => {
+    logged.value = await checkPswd(event.target.value);
+  });
+
   return (
     <>
       <p>private conversations here</p>
+      <input type="text" onInput$={onInput} />
+      <p>user is logged: {logged.value.toString()}</p>
     </>
   );
 });
